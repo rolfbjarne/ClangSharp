9baa67566e5df5bdd6958de28d2b51433f0fadb7
Rolf Bjarne Kvinge <rolf@xamarin.com>
2025-12-02 14:31:55 +0100

    Add support for protocols in @interface definitions (ObjCInterfaceDecl).

--- sources/libClangSharp/ClangSharp.cpp
+++ sources/libClangSharp/ClangSharp.cpp
@@ -3615,6 +3615,15 @@ int clangsharp_Cursor_getNumProtocols(CXCursor C) {
         if (const ObjCProtocolDecl* OCPD = dyn_cast<ObjCProtocolDecl>(D)) {
             return OCPD->protocol_size();
         }
+
+        if (const ObjCInterfaceDecl* OCID = dyn_cast<ObjCInterfaceDecl>(D)) {
+            unsigned n = 0;
+
+            for (auto protocol : OCID->protocols()) {
+                n++;
+            }
+            return n;
+        }
     }
 
     return -1;
@@ -3953,6 +3962,17 @@ CXCursor clangsharp_Cursor_getProtocol(CXCursor C, unsigned i) {
             unsigned n = 0;
 
             for (auto protocol : OCPD->protocols()) {
+                if (n == i) {
+                    return MakeCXCursor(protocol, getCursorTU(C));
+                }
+                n++;
+            }
+        }
+
+        if (const ObjCInterfaceDecl* OCID = dyn_cast<ObjCInterfaceDecl>(D)) {
+            unsigned n = 0;
+
+            for (auto protocol : OCID->protocols()) {
                 if (n == i) {
                     return MakeCXCursor(protocol, getCursorTU(C));
                 }
