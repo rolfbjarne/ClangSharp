c063a9ffbfa7e36d3afb2439c0253baba12d424a
Rolf Bjarne Kvinge <rolf@xamarin.com>
2025-12-04 15:43:34 +0100

    Expose ObjCTypeParamDecl->hasExplicitBound|getVariance to ClangSharp.

---
+++ sources/ClangSharp.Interop/clang/ObjCTypeParamVariance.cs
@@ -0,0 +1,15 @@
+// Copyright (c) .NET Foundation and Contributors. All Rights Reserved. Licensed under the MIT License (MIT). See License.md in the repository root for more information.
+
+// Ported from https://github.com/llvm/llvm-project/tree/llvmorg-20.1.2/clang/include/clang-c
+// Original source is Copyright (c) the LLVM Project and Contributors. Licensed under the Apache License v2.0 with LLVM Exceptions. See NOTICE.txt in the project root for license information.
+
+using System;
+
+namespace ClangSharp.Interop;
+
+#pragma warning disable CA1028 // If possible, make the underlying type of ObjCTypeParamVariance System.Int32 instead of byte
+public enum ObjCTypeParamVariance : byte {
+    Invariant,
+    Covariant,
+    Contravariant,
+}
--- sources/libClangSharp/ClangSharp.cpp
+++ sources/libClangSharp/ClangSharp.cpp
@@ -5078,6 +5078,33 @@ CXCursor clangsharp_Cursor_getUsedContext(CXCursor C) {
     return clang_getNullCursor();
 }
 
+CLANGSHARP_LINKAGE unsigned clangsharp_Cursor_getTypeParamHasExplicitBound(CXCursor C)
+{
+    if (isDeclOrTU(C.kind)) {
+        const Decl* D = getCursorDecl(C);
+
+        if (const ObjCTypeParamDecl* OCTPD = dyn_cast<ObjCTypeParamDecl>(D)) {
+            return OCTPD->hasExplicitBound();
+        }
+    }
+
+    return 0;
+}
+
+CLANGSHARP_LINKAGE ObjCTypeParamVariance clangsharp_Cursor_getTypeParamVariance(CXCursor C)
+{
+    if (isDeclOrTU(C.kind)) {
+        const Decl* D = getCursorDecl(C);
+
+        if (const ObjCTypeParamDecl* OCTPD = dyn_cast<ObjCTypeParamDecl>(D)) {
+            return OCTPD->getVariance();
+        }
+    }
+
+    return (ObjCTypeParamVariance) 0;
+}
+
+
 CXCursor clangsharp_Cursor_getVBase(CXCursor C, unsigned i) {
     if (isDeclOrTU(C.kind)) {
         const Decl* D = getCursorDecl(C);
